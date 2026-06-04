9bb6a3e91e44ec1e12937a9617c9a8f54684c68e
user <email>
2026-06-04 13:31:50 +0300

    Fix validator for query params

--- backend/utils/validators.py
+++ backend/utils/validators.py
@@ -1,3 +1,5 @@
+from urllib.parse import urlparse, parse_qs
+
 from config import settings
 
 
@@ -14,16 +16,17 @@ def validate_query_params(url: str) -> dict | None:
     default_limit: int = 10
     default_page: int = 1
     default_direction: str = "desc"
+    query_params = {}
 
     parsed_url = urlparse(url)
-    params = parse_qs(parsed_url.query)
-    query_params = {}
-    try:
-        query_params['page'] = int(params['page'][0]) if params['page'][0].isdigit() else default_page
-        query_params['limit'] = int(params['limit'][0]) if params['limit'][0].isdigit() else default_limit,
-        query_params['direction'] = params['direction'][0].lower() if (params['direction'][0].lower() == "asc"
-                                                                       or params['direction'][0].lower() == "desc") \
-            else default_direction
-    except KeyError:
-        query_params = None
+    if parsed_url.path == '/api':
+        params = parse_qs(parsed_url.query)
+        try:
+            query_params['page'] = int(params['page'][0]) if params['page'][0].isdigit() else default_page
+            query_params['limit'] = int(params['limit'][0]) if params['limit'][0].isdigit() else default_limit,
+            query_params['direction'] = params['direction'][0].lower() if (params['direction'][0].lower() == "asc"
+                                                                           or params['direction'][0].lower() == "desc") \
+                else default_direction
+        except KeyError:
+            query_params = None
     return query_params
